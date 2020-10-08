57bdbe816361c1839100dfa5f0fff1bb426dc2c8
leveses <boda.zsombor10@gmail.com>
2020-10-08 09:24:31 +0200

    cimegyelno metódus elkészítése

--- DHCPSzimulacio/Program.cs
+++ DHCPSzimulacio/Program.cs
@@ -3,13 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace DHCPSzimulacio
 {
     class Program
     {
+        static List<string> excluded = new List<string>();
         static void Main(string[] args)
         {
+            BeolvasExcluded();
+            Console.ReadKey();
+        }
+        static void BeolvasExcluded()
+        {
+            
+            try
+            {
+                StreamReader file = new StreamReader("excluded.csv");
+                try
+                {
+                    while (!file.EndOfStream)
+                    {
+                        excluded.Add(file.ReadLine());
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    file.Close();
+                }
+                file.Close();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static string CimEgyelNo(string cim)
+        {
+            string[] adat = cim.Split('.');
+            int utolso = int.Parse(adat[3]);
+            if (utolso<255)
+            {
+                utolso++;
+            }
+            return adat[0] + "." + adat[1] + "." + adat[3] + "." + utolso.ToString();
         }
     }
 }
